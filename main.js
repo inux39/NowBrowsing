da12d645c3979efa2f31da0308d822bd1bddd269
inux39 <inux39@protonmail.ch>
2018-06-12 12:11:40 +0900

    text separate with comment

--- main.js
+++ main.js
@@ -3,30 +3,31 @@ const DEF_HEAD = "Now Browsing";
 window.onload = function() {
 	var head = DEF_HEAD;
 	var body = "";
-	var text = "";
 	var url = "";
+	var comment = "";
 	var features = "width=600,height=500,centerscreen=yes,menubar=yes,location=yes,resizeable=yes,scrollbars=yes,status=yes";
 	browser.tabs.query({currentWindow: true, active: true})
 		.then((tabs) => {
 			body = tabs[0].title;
 			url = tabs[0].url;
-			flush(head, body, url);
+			flush(head, body, url, comment);
 		})
 		.catch(clear);
 
-	document.getElementById("text").oninput = function() {
-		text = document.getElementById("text").value;
+	document.getElementById("comment").oninput = function() {
+		comment = document.getElementById("comment").value;
+		flush(head, body, url, comment);
 	}
 
 	document.getElementById("hash").onchange = function() {
 		head = noSpace(head);
-		flush(head, body, url);
+		flush(head, body, url, comment);
 	}
 
 	document.getElementById("head").oninput = function() {
 		head = document.getElementById("head").value;
 		head = noSpace(head);
-		flush(head, body, url);
+		flush(head, body, url, comment);
 	}
 
 	document.getElementById("mastodon").onclick = function() {
@@ -51,17 +52,21 @@ function noSpace(head) {
 	return head;
 }
 
-	} else {
-		head = head.substr(1, head.length);
+function fit(text, max) {
+	var s = text;
+	if(text.length < max) {
+		s = text.substr(0, max - 3) + "...";
 	}
+	return s;
 }
 
-function flush(head, body, url) {
-	document.getElementById("text").value = head + ": " + body + "\n" + url + "\n";
+function flush(head, body, url, comment) {
+	var t = head + ": " + body + "\n" + comment;
+	document.getElementById("preview").value = t;
 }
 
 function clear() {
-	document.getElementById("text").value = "";
+	document.getElementById("preview").value = "";
 }
 
 })();
