7041a4c16822e80643cc5b672b724edacb64df02
inux39 <inux39@protonmail.ch>
2018-06-15 23:01:00 +0900

    Better Design

--- main.js
+++ main.js
@@ -115,7 +115,7 @@ function fit(text, max) {
 }
 
 function flush(text) {
-	document.getElementById("count").innerHTML = text.length;
+	document.getElementById("count").innerText = text.length;
 	document.getElementById("preview").value = text;
 }
 
