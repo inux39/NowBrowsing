582168566a71b081c9265040bd6b87eac8a0c6d6
inux39 <inux39@protonmail.ch>
2019-12-23 12:41:04 +0900

    oh, mama

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ window.onload = function() {
     var body = '';
     var url = '';
     var comment = '';
+
+    var storage = browser.storage.sync.get('button');
+    storage.then((ret) => {
+        document.getElementById('comment').value = ret.button;
+    });
+
     /*
     browser.tabs.query({currentWindow: true, active: true})
         .then((tabs) => {
@@ -16,6 +22,13 @@ window.onload = function() {
         })
         .catch(clear);
     */
+}
+
+document.getElementById('button1').onclick = function() {
+    var input = document.getElementById('comment').value;
+    browser.storage.sync.set({
+        button: 'a=b'
+    });
 }
 //document.addEventListener('DOMContentLoaded', restoreOptions);
 })();
