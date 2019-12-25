c1a96bbe04716894c423764f0bc06cf9a897c23c
inux39 <inux39@protonmail.ch>
2019-12-25 13:36:57 +0900

    up

--- src/index.js
+++ src/index.js
@@ -5,32 +5,33 @@
 //});
 
 window.onload = function() {
-    var body = '';
-    var url = '';
-    var comment = '';
-
-    var storage = browser.storage.sync.get('button');
-    storage.then((ret) => {
-        document.getElementById('share_text').value = ret.button;
-    });
-
     /*
-    browser.tabs.query({currentWindow: true, active: true})
-        .then((tabs) => {
-            body = tabs[0].title;
-            url = tabs[0].url;
-        })
-        .catch(clear);
+    var storage = browser.storage.sync.get("button");
+    storage.then((ret) => {
+        document.getElementById("share_text").value = ret.button;
+    });
     */
+
+    var body;
+    var url;
+    var textarea = document.getElementById("share_text");
+    browser.tabs.query({currentWindow: true, active: true})
+    .then((tabs) => {
+        body = tabs[0].title;
+        url = tabs[0].url;
+        textarea.innerHTML = body + "\n" + url;
+    });
 }
 
-document.querySelector("share_button").addEventListener('onclick', function(e) {
-    var input = document.getElementById('share_text').value;
+/*
+document.querySelector("share_button").addEventListener("onclick", function(e) {
+    var input = document.getElementById("share_text").value;
     browser.storage.sync.set({
-        button: 'a=b'
+        button: "a=b"
     });
     e.preventDefault();
 });
+*/
 /*
 document.getElementById('button1').onclick = function() {
     var input = document.getElementById('comment').value;
--- src/settings.js
+++ src/settings.js
@@ -2,38 +2,38 @@
 var ss = 0;
 
 function build_accountlist() {
-    var frame = document.createElement('li');
-    frame.setAttribute('class', 'account_item');
+    var frame = document.createElement("li");
+    frame.setAttribute("class", "account_item");
 
-    var account_selected = document.createElement('input');
-    account_selected.setAttribute('type', 'checkbox');
-    account_selected.setAttribute('class', 'account_selected');
+    var account_selected = document.createElement("input");
+    account_selected.setAttribute("type", "checkbox");
+    account_selected.setAttribute("class", "account_selected");
     frame.appendChild(account_selected);
 
-    var user_icon = document.createElement('img');
-    user_icon.setAttribute('class', 'user_icon');
-    user_icon.setAttribute('alt', "user's icon");
+    var user_icon = document.createElement("img");
+    user_icon.setAttribute("class", "user_icon");
+    user_icon.setAttribute("alt", "user"s icon");
     frame.appendChild(user_icon);
 
-    var usernames = document.createElement('div');
-    usernames.setAttribute('class', 'usernames');
-    var user_name = document.createElement('span');
-    user_name.setAttribute('class', 'user_name');
+    var usernames = document.createElement("div");
+    usernames.setAttribute("class", "usernames");
+    var user_name = document.createElement("span");
+    user_name.setAttribute("class", "user_name");
     usernames.appendChild(user_name);
-    var user_id = document.createElement('span');
-    user_id.setAttribute('class', 'user_id');
+    var user_id = document.createElement("span");
+    user_id.setAttribute("class", "user_id");
     usernames.appendChild(user_id);
-    usernames.appendChild(document.createElement('br'));
-    var host = document.createElement('span');
-    host.setAttribute('class', 'host');
+    usernames.appendChild(document.createElement("br"));
+    var host = document.createElement("span");
+    host.setAttribute("class", "host");
     usernames.appendChild(host);
     frame.appendChild(usernames);
 
-    var refresh_button = document.createElement('input');
-    refresh_button.setAttribute('class', 'refresh_button');
-    refresh_button.setAttribute('type', 'image');
-    refresh_button.setAttribute('src', 'img/reload.svg');
-    refresh_button.setAttribute('aria-label', 'Refresh account profiles');
+    var refresh_button = document.createElement("input");
+    refresh_button.setAttribute("class", "refresh_button");
+    refresh_button.setAttribute("type", "image");
+    refresh_button.setAttribute("src", "img/reload.svg");
+    refresh_button.setAttribute("aria-label", "Refresh account profiles");
     frame.appendChild(refresh_button);
 
     return frame
@@ -41,21 +41,21 @@ function build_accountlist() {
 
 function test() {
     var child = build_accountlist();
-    child.getElementsByClassName('user_name')[0].innerText = "inux39";
-    var o = document.getElementById('account_list');
+    child.getElementsByClassName("user_name")[0].innerText = "inux39";
+    var o = document.getElementById("account_list");
     o.appendChild(child);
 }
 
 window.onload = function() {
 }
 
-document.getElementById('button_clear_all').addEventListener("click", clear_all);
-document.getElementById('button_add').addEventListener("click", function() {
+document.getElementById("button_clear_all").addEventListener("click", clear_all);
+document.getElementById("button_add").addEventListener("click", function() {
 });
-document.getElementById('button_remove').addEventListener("click", function() {
-    var check = document.getElementsByClassName('account_selected');
-    var items = document.getElementById('account_items');
-    var confirmed = window.confirm('Realy ok?');
+document.getElementById("button_remove").addEventListener("click", function() {
+    var check = document.getElementsByClassName("account_selected");
+    var items = document.getElementById("account_items");
+    var confirmed = window.confirm("Realy ok?");
     // 削除するアカウントのリスト
     // できればこれを警告で出したい
     var list = [];
@@ -83,7 +83,7 @@ function modal_open() {
 function modal_close() {
     var mask = document.getElementById("modal_overlay");
     var modal = document.getElementById("modal");
-    var confirmed = window.confirm('Realy ok?');
+    var confirmed = window.confirm("Realy ok?");
     if(confirmed) {
         mask.classList.add("modal_hidden");
         modal.classList.add("modal_hidden");
