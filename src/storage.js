fcc6da919913b4a88797e2d779a97826f35f906d
inux39 <inux39@protonmail.ch>
2020-01-29 00:04:51 +0900

    あとで英語とかに対応するので、いちおう日本語に

--- src/storage.js
+++ src/storage.js
@@ -14,7 +14,7 @@ function save_to_local(e, option) {
 }
 
 function clear_all() {
-    const confirmed = window.confirm('All configuration will wipe.\nOK?');
+    const confirmed = window.confirm('全ての情報を削除します');
     if(confirmed) {
         browser.storage.sync.clear();
         browser.storage.local.clear();
