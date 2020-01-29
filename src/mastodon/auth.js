57aef65d6a554e43665ce65cf0903dc953c220a4
inux39 <inux39@protonmail.ch>
2020-01-30 01:49:05 +0900

    Object Prototype

--- src/mastodon/auth.js
+++ src/mastodon/auth.js
@@ -1,15 +1,47 @@
-(function(){
-const redirect = browser.identity.getRedirectURL();
-const appName = "Now Browsing";
-const appURL = "https://github.com/inux39/NowBrowsing";
-const scopes = ["read", "write"];
-const auth_uri = `/oauth/authorize/\
-?client_id=${client_id}\
-&scope=${encodeURIComponent(scopes.join('+'))}\
-&redirect_uri=${encodeURIComponent(redirect)}\
-&response_type=code`;
-const token_uri = "/oauth/token";
-const revoke_uri = "/oauth/revoke";
+var Mastodon = function(domain) {
 
-})();
+this.domain = domain;
+this.redirect = browser.identity.getRedirectURL();
+this.appName = "Now Browsing";
+this.appURL = "https://github.com/inux39/NowBrowsing";
+this.scopes = ["read", "write"];
+this.endpoint = {
+    token: "/oauth/token",
+    revoke: "/oauth/revoke"
+};
+
+this.authentication = function() {
+    const auth_uri = domain + `/oauth/authorize/\
+    ?client_id=${client_id}\
+    client_secret=${client_secret}\
+    redirect_uri=${redirect}\
+    grant_type=`;
+};
+
+this.createApp = function() {
+    const request = new XMLHttpRequest();
+    const url = domain + "/oauth/v1/apps"
+    request.open("POST", url);
+    request.setRequestHeader("Content-Type", "application/json");
+};
+
+this.validate = function(redirectURL) {
+
+};
+
+this.authorize = function() {
+    return browser.identity.launchWebAuthFlow({
+        interactive: true,
+        url: domain + auth_uri
+    });
+};
+
+this.getAccessToken = function() {
+    console.log(domain);
+    return authorize();
+};
+//&scope=${encodeURIComponent(scopes.join('+'))}\
+//&redirect_uri=${encodeURIComponent(redirect)}\
+//&response_type=code;
+};
 
